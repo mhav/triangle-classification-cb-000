fef24578dcea95034f82f941f3aa31151b855d58
Marcus Havanek <marcus.havranek@havranek.co.at>
2017-07-15 10:11:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if @a == 0 or @b == 0 or @c == 0
+      begin
+        raise TriangleError
+      end
+    elsif @a < 0 or @b < 0 or @c < 0
+      begin
+        raise TriangleError
+      end
+    elsif @a >= @b + @c or @b >= @a + @c or @c >= @a + @b
+      begin
+        raise TriangleError
+      end
+    elsif @a == @b and @a == @c and @b == @c
+      :equilateral
+    elsif @b == @c or @a == @c or @a == @b
+      :isosceles
+    elsif @a != @b and @a != @c and @b != @c
+      :scalene
+    #else
+
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+
 end
